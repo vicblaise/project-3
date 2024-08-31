77869b7d915e2e8bab4fdf3e1a86398afee6c799
Victor Ogwuche <vblaise967@gmail.com>
2024-08-31 23:41:40 +0000

    Add code for win or draw

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ while True:
     else:
             choice_name = 'scissors'
 
-         
+    
     print('Your choice is \n', choice_name)
     print('Computers Turn....')
     """
@@ -41,6 +41,53 @@ while True:
         print("Computers choice is \n", comp_choice_name)
         print(choice_name, "vs", comp_choice_name)
 
+#checking for possible draw
+
+        if choice == comp_choice:
+            print("No winner", end = "")
+            result = 'DRAW'
+
+# checking for possible win
+        
+        if (choice==1 and comp_choice == 2):
+            print('paper wins', end ="")
+            result = 'Paper'
+
+        elif(choice == 2 and comp_choice == 1):
+            print("Paper wins", end = "")
+            result ='Paper'
+
+        if (choice == 1 and comp_choice == 3):
+            print("Rock wins", end ="")
+            result = 'Rock'
+
+        elif (choice == 3 and comp_choice == 1):
+            print("Rock wins", end = "")
+            result = 'Rock'
+
+        if (choice == 2 and comp_choice == 3):
+            print("Scissors wins", end = "")
+            result = Scissors
+
+        elif(choice == 3 and comp_choice == 2):
+            print("Scissors wins", end = "")
+            result = Rock
 
 
+#showing results for either user or computer win or draw
+        if result == 'Draw':
+            print("...its a Draw...")
 
+        if result == choice_name:
+            print("...User wins...")
+
+        else:
+            print("...Computer wins...")
+        print("Would you like to try agin? (Y/N)")
+
+        your_ans = input().lower()
+        if your_ans == 'n':
+            break
+
+
+print("hope you had fun? BYE")
